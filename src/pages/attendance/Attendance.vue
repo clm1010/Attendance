ea2ec544aadb3191cd25bd6379e87c2ba8f0beed
CLM <limin_chu@163.com>
2018-06-03 20:30:54 +0800

    Attendance
    Attendance

--- src/pages/attendance/Attendance.vue
+++ src/pages/attendance/Attendance.vue
@@ -1,7 +1,14 @@
 <template>
 <div>
   <attendance-header></attendance-header>
-  <attendance-calendar></attendance-calendar>
+  <attendance-calendar
+    ref="AttendanceCalendar"
+    :markDateMore="arr"
+    v-on:choseDay="clickDay"
+    v-on:changeMonth="changeDate"
+    agoDayHide='1517483961'
+    futureDayHide='1536054400'
+  ></attendance-calendar>
 </div>
 </template>
 
@@ -13,8 +20,61 @@ export default {
   components: {
     AttendanceHeader,
     AttendanceCalendar
+  },
+  data () {
+    return {
+      arr: [{
+        date: '2018-4-1',
+        className: 'mark1'
+      }, {
+        date: '2018-4-2',
+        className: 'mark1'
+      }, {
+        date: '2018-4-13',
+        className: 'mark2'
+      }]
+    }
+  },
+  methods: {
+    clickDay (data) {
+      console.log('选中了', data) // 选中某天
+      // this.$toast('选中了' + data)
+    },
+    clickToday (data) {
+      console.log('跳到了本月', data) // 跳到了本月
+    },
+    changeDate (data) {
+      // this.$toast('切换到的月份为' + data)
+      console.log('左右点击切换月份', data) // 左右点击切换月份
+    },
+    demo () {
+      // this.$refs.Calendar.ChoseMonth('2017-12-12',false); // 跳到12月12日 但是不选中12月12日
+      this.$refs.Calendar.ChoseMonth('2017-12-12') // 跳到12月12日
+    }
+  },
+  created () {
+    function format (date, index) {
+      date = new Date(date)
+      return `${date.getFullYear()}-${date.getMonth() + 1}-${index}`
+    }
+    this.arr = [{
+      date: format(new Date(), 1),
+      className: 'mark1'
+    }, {
+      date: format(new Date(), 2),
+      className: 'mark1'
+    }, {
+      date: format(new Date(), 13),
+      className: 'mark2'
+    }]
   }
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.wh_container >>>
+  .mark1
+    background-color: red;
+  .mark2
+    background-color: blue;
+</style>
